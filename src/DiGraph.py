36aaa7761f85a15a7bb326f753c88dde5187e054
dvirs <dvir.shaul@gmail.com>
2020-12-30 13:31:22 +0200

    second check

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,6 +1,7 @@
 import GraphInterface
 
 
+
 class DiGraph(GraphInterface):
     """This abstract class represents an interface of a graph."""
 
@@ -50,6 +51,7 @@ class DiGraph(GraphInterface):
 
         Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
         """
+
         raise NotImplementedError
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
